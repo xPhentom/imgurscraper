778911e6e26044dac87fb0752034917e3982ab2c
root <wouteroozeleer@gmail.com>
2016-01-14 11:01:24 +0100

    Finished

--- imgurscraper.py
+++ imgurscraper.py
@@ -1,15 +1,29 @@
 #!/bin/python
 import string
 import random
+import urllib
 
 def id_generator(size=7, chars=string.ascii_uppercase + string.digits + string.ascii_lowercase):
 
     identity =  ''.join(random.choice(chars) for _ in range(size))
-
+    
     print identity
 
-id_generator()
+    picture = urllib.URLopener()
+    try:
+        picture.retrieve("http://i.imgur.com/%s.png" % identity, "%s.png" % identity)
+        print "Downloaded %s.png" % identity
+    except:
+        pass
 
-#TODO: save file from site, check for jpg and png
+
+print "How many images do you want to try and download?"
+ans = raw_input(">>>   ")
+
+
+for x in range(0,int(float(ans))):
+    id_generator()
+
+    
 
 
